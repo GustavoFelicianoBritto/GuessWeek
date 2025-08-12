122b6b07bb0782c45de498b0ca6acf2558c36a19
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-08-12 20:26:21 -0300

    Versão adivinhar dia semana (futuro)

---
+++ src/calendar/Library.java
@@ -0,0 +1,119 @@
+package calendar;
+import java.time.*;
+
+public class Library {
+
+
+	public static int diasTotaisdeUmAno(int ano) //Verificando pelo ano se ele tem 365 ou 366 dias
+	{
+		return (ano%4==0 && ano %100!=0) || ano%400==0? 366:365; 
+	}
+	public static boolean verificacaoDeAnoBissexto(int ano) //Verificação booleana se é ou não bissexto
+	{
+		return (ano%4==0 && ano %100!=0) || ano%400==0;
+	}
+	
+	public static int diasTotaisdoMes(int mesAtual,int ano) //verificando pelo index se o mês em questão tem 28,29,30 ou 31 dias
+	{
+		int diasMes[]= {0,31,28,31,30,31,30,31,31,30,31,30,31};
+		
+		if(verificacaoDeAnoBissexto(ano) && mesAtual==2) //Se for bissexto, e for o mês 2 (fevereiro), vai ter acrescimo de 1 dia
+		{
+			return diasMes[mesAtual]+1;
+		}
+		else {
+			return diasMes[mesAtual];
+		}
+		
+		
+	}
+	
+	public static int diasRestantesAteFimdoAno(int dia ,int mes,int ano) //Método pra saber quantos dias até o fim do ano
+	{
+		int diasAno = diasTotaisdeUmAno(ano); //365 ou 366
+		
+		int diasRestantes= diasTotaisdoMes(mes,ano)-dia; //dias restantes do mês baseado no dia atual, exemplo: dia 2, faltam 28 para 30
+		
+		//System.out.println(diasRestantes);
+		for(int i=mes+1;i<=12;i++) //Começa próximo mês, vai até dezembro e soma todos dias do ano
+		{	
+			diasRestantes+=diasTotaisdoMes(i,ano);
+			//System.out.println(i);
+			//System.out.println(diasRestantes);
+			
+		}
+		return diasRestantes; // retorna os dias totais até o final do ano
+	}
+	
+
+	public static int diferencaDentroAno(int dia1 ,int mes1,int ano1,int dia2 ,int mes2,int ano2)//usar apenas para a diferença dentro de um mesmo ano
+	{
+		int diasrestantesDia1 = diasRestantesAteFimdoAno(dia1, mes1, ano1);
+		int diasrestantesDia2 = diasRestantesAteFimdoAno(dia2, mes2, ano2);
+		int resultado=0;
+		
+		
+		resultado= diasrestantesDia1 - diasrestantesDia2;
+		
+		//System.out.println(resultado);
+		
+		return resultado;
+		
+	}
+	
+
+	public static void diferenceBetwenDays(int targetDay,int targetMonth, int targetYear)
+	{
+		LocalDate dateUser = LocalDate.now(); //pega a data do pc do usuario no formato yyyy/mm/dd
+		
+		int diaBase= dateUser.getDayOfMonth(); int diaMeta=targetDay;
+		int mesBase= dateUser.getMonthValue(); int mesMeta=targetMonth;
+		int anoBase= dateUser.getYear(); int anoMeta= targetYear;
+		
+		
+		int diasRestantes=0;
+	
+		diasRestantes += diasRestantesAteFimdoAno(diaBase, mesBase, anoBase);
+		
+		if(anoBase!=anoMeta)
+		{
+			//System.out.println("Debug");
+			//System.out.println(diasRestantes);
+			
+			for(int i=anoBase+1;i<anoMeta;i++)
+			{
+				
+				diasRestantes+= diasTotaisdeUmAno(i);
+				
+			}
+			
+			diasRestantes+= (diasTotaisdeUmAno(anoMeta)-diasRestantesAteFimdoAno(diaMeta, mesMeta, anoMeta));
+		}
+		else
+		{
+			//System.out.println("Debug 2");
+			diasRestantes = diferencaDentroAno(diaBase, mesBase, anoBase, diaMeta, mesMeta, anoMeta);
+		}
+		
+		
+		
+		guessWeek(diasRestantes, diaBase, mesBase, anoBase, diaMeta, mesMeta, anoMeta);
+		
+	}
+	
+	public static void guessWeek(int diference,int diaAtual,int mesAtual,int anoAtual, int diaMeta,int mesMeta,int anoMeta)
+	{
+		LocalDate dateUser = LocalDate.now();
+		
+		DayOfWeek weekDayUser = dateUser.getDayOfWeek();
+		System.out.println(diference);
+		int resto = diference%7;
+		
+		DayOfWeek weekDayTarget = weekDayUser.plus(resto);
+		
+		System.out.printf("Hoje é %d/%d/%d, uma %s\nNo dia meta %d/%d/%d será %s",diaAtual,mesAtual,anoAtual,weekDayUser,diaMeta,mesMeta,anoMeta,weekDayTarget);
+			
+	}
+	
+}
+
---
+++ src/calendar/Main.java
@@ -0,0 +1,12 @@
+package calendar;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+	Library.diferenceBetwenDays(31, 12, 2025);
+		
+	}
+
+}
